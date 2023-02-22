2660f977a1b8504d6ef708b93e8790360dcaf2f0
Aldo Silvestre <aldosilvestre89@gmail.com>
2023-02-22 01:42:39 -0300

    add plugin wilder ufo surround noice

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ return {
   'kyazdani42/nvim-web-devicons',
   'norcalli/nvim-colorizer.lua',
   'glepnir/galaxyline.nvim',
-  'AndrewRadev/splitjoin.vim', --> switching between a single-line statement and a multi-line one
   'christoomey/vim-tmux-navigator', --> tmux
   { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup() end }, --> autopairs
   'terryma/vim-multiple-cursors', --> multiple cursor
@@ -14,6 +13,6 @@ return {
       require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     end
   },
-
-  'norcalli/nvim-colorizer.lua' --> css colors
+  'norcalli/nvim-colorizer.lua', --> css colors
+  'folke/which-key.nvim'
 }
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("tokyonight").setup({
-      style = "Storm",
+      style = "Night",
       transparent = true, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
@@ -15,7 +15,7 @@ return {
         sidebars = "transparent", -- style for sidebars, see below
         floats = "transparent", -- style for floating windows
       },
-      sidebars = { "qf", "vista_kind", "terminal", "packer" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      sidebars = { "qf", "vista_kind", "terminal", "packer", "lazy" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
       day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
       dim_inactive = false, -- dims inactive windows
