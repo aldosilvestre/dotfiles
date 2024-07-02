bc225bc045786e5b2d785e19153aef52f109b545
Aldo Silvestre <aldosilvestre89@gmail.com>
2024-07-02 01:05:12 -0300

    [Mod] - Added kulala + test splitjoin

---
+++ .config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,23 @@
+-- HTTP REST-Client Interface
+vim.filetype.add({
+  extension = {
+    ['http'] = 'http',
+  },
+})
+
+return {
+  'mistweaverco/kulala.nvim',
+  event = "VeryLazy",
+  config = function()
+    -- Setup is required, even if you don't pass any options
+    require('kulala').setup({
+      -- default_view, body or headers
+      default_view = "body",
+      -- dev, test, prod, can be anything
+      -- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
+      default_env = "dev",
+      -- enable/disable debug mode
+      debug = false,
+    })
+  end
+}
--- .config/nvim/lua/plugins/splitjoin.lua
+++ .config/nvim/lua/plugins/splitjoin.lua
@@ -1,13 +1,23 @@
+-- return {
+--   'echasnovski/mini.splitjoin', --> Split join sentences
+--   event = "VeryLazy",
+--   version = '*',
+--   opts = {
+--     mappings = {
+--       toggle = 'gs',
+--       split = '',
+--       join = '',
+--     }
+--   },
+--   config = true
+-- }
 return {
-  'echasnovski/mini.splitjoin', --> Split join sentences
-  event = "VeryLazy",
-  version = '*',
-  opts = {
-    mappings = {
-      toggle = 'gs',
-      split = '',
-      join = '',
-    }
-  },
-  config = true
+  'Wansmer/treesj',
+  keys = { 'gs' },
+  config = function()
+    require('treesj').setup({
+      use_default_keymaps = true,
+    })
+    vim.keymap.set('n', 'gs', require('treesj').toggle)
+  end,
 }
